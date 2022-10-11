0e13eac8c658547bc46d9ea3275d7038c8f6c0a1
Züleyha Kurt <netice2017@gmail.com>
2022-10-11 20:02:42 +0300

    edit

--- img-gallery/src/App.js
+++ img-gallery/src/App.js
@@ -4,6 +4,7 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
+      <h1>Image Gallery</h1>
       <div className="container">
         {data.map((item, index) => <Card key={index} item={item} />)}
         </div>
